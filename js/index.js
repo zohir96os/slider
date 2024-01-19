dd5f7b084cc50bb8146086e60a9991223272a9ff
zohiros <zohir96os@gmail.com>
2024-01-19 10:51:57 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,18 @@
+//Hundel slider
+let rangeSlider = document.querySelector("input[type='range']");
+let price = document.querySelector(".plan span");
+
+rangeSlider.addEventListener("change", (e) => {
+  var value = e.target.value;
+  price.innerHTML = value;
+});
+
+//Hundle discount
+let btnSwitch = document.querySelector(".switch");
+let toggle = document.querySelector(".switch span");
+btnSwitch.addEventListener("click", (e) => {
+  toggle.classList.toggle("switched");
+  if (toggle.classList.contains("switched")) {
+    price.innerHTML *= 0.025;
+  }
+});
